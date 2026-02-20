a9108e686d8998375bff6f66a872fa65e0190b1e
Tarun-Gup <45903018+Tarun-Gup@users.noreply.github.com>
2026-02-20 23:31:48 +0100

    Sitemap fix

---
+++ sitemap.xml.11ty.js
@@ -0,0 +1,40 @@
+module.exports = class {
+  data() {
+    return {
+      permalink: "/sitemap.xml",
+      layout: false,
+      eleventyExcludeFromCollections: true
+    };
+  }
+
+  render(data) {
+    const baseUrl = data.site?.url || "https://analystinaction.netlify.app";
+
+    const ignore = new Set([
+      "/sitemap.xml",
+      "/search/",
+      "/README/",
+      "/posts/TEMPLATE/"
+    ]);
+
+    const pages = data.collections?.all || [];
+
+    const urlNodes = pages
+      .filter(p => p.url && !ignore.has(p.url))
+      .map(p => {
+        const loc = `${baseUrl}${p.url}`;
+        const lastmod = p.date ? new Date(p.date).toISOString() : null;
+
+        return `  <url>
+    <loc>${loc}</loc>${lastmod ? `\n    <lastmod>${lastmod}</lastmod>` : ""}
+  </url>`;
+      })
+      .join("\n");
+
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urlNodes}
+</urlset>
+`;
+  }
+};
